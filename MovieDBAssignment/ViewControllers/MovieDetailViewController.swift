885a4b3d7ce73d6a26add6ef44c59a6befa2b15e
Vikash Kumar <vikashkumar2804@gmail.com>
2017-08-21 18:08:58 +0530

    changes

--- MovieDBAssignment/ViewControllers/MovieDetailViewController.swift
+++ MovieDBAssignment/ViewControllers/MovieDetailViewController.swift
@@ -72,19 +72,6 @@ class MovieDetailViewController: UIViewController {
         posterimageView.kf.setImage(with: URL(string: imageUrl ), placeholder: nil, options: nil, progressBlock: nil, completionHandler: nil)
         
     }
-    
         
-   
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
